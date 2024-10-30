333008cdae98ad91b306ec2491e7d3bd220918ae
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-10-30 21:15:35 +0100

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""A python module"""
+
+from base_caching import BaseCaching
+# base_caching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """Inherits from base caching"""
+    def __init__():
+        """Instance of the class"""
+        super().__init__()
+        self.cache_data = OrderDict()
+
+    def put(self, key, item):
+        """put method"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item, _ = self.cache_data.popitem(False)
+            print("DISCARD!", first_item)
+
+    def get(self, key):
+        """geth method"""
+        retrun self.cache_data.get(key, None)
+
